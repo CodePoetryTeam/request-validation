165bfd1418ac0731ffbd8d5e331eef1e9312a610
codepoetry <56636379+codepoetryindia@users.noreply.github.com>
2023-09-09 15:29:13 +0530

    Init

---
+++ src/RequestServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+namespace Maansu\RequestValidation;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\ServiceProvider;
+
+class RequestServiceProvider extends ServiceProvider
+{
+
+    public function register(): void
+    {
+        $this->commands([
+            \Maansu\RequestValidation\Console\RequestMakeCommand::class
+        ]);
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->app->afterResolving(RequestAbstract::class, function ($resolved) {
+            $resolved->validateResolved();
+        });
+
+        $this->app->resolving(RequestAbstract::class, function ($request, $app) {
+            $request = RequestAbstract::createFrom($app['request'], $request);
+            $request->setContainer($app);
+        });
+    }
+}
